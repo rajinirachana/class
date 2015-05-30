becc065d4388ae383750a69d0e5719ad764c5eec
Rajini Mudrakolla <Rajini1@Rajinis-MacBook-Pro.local>
2015-05-30 21:00:10 +0100

    Initial commit

---
+++ src/main/java/class/com/App.java
@@ -0,0 +1,13 @@
+package class.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
